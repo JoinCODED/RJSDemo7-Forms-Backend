a9f61dec4cf60cc2aa8da2069ca75e29556f086d
lailalelouch <39729644+lailalelouch@users.noreply.github.com>
2019-03-11 00:19:42 +0300

    Update views.py

--- forms/views.py
+++ forms/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Person
 from .serializers import PersonSerializer
-# Create your views here.
-class PersonViewSet(viewsets.ModelViewSet):
 
+
+class PersonViewSet(viewsets.ModelViewSet):
 	serializer_class = PersonSerializer
 	queryset = Person.objects.all()
